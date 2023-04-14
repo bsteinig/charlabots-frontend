5d4dbff63a8b1cf686116f82bf9085b93fddc617
Benjamin Steinig <bsteinig@umich.edu>
2023-04-14 04:43:31 -0400

    Added code highlighting.

--- src/pages/create/[id].js
+++ src/pages/create/[id].js
@@ -10,7 +10,14 @@ import {
   createStyles,
 } from "@mantine/core";
 import { useForm } from "@mantine/form";
+import { RichTextEditor } from "@mantine/tiptap";
 import { IconDeviceFloppy, IconHome2 } from "@tabler/icons-react";
+import { useEditor } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
+import CodeBlockLowlight from "@tiptap/extension-code-block-lowlight";
+import { CharlaBotsHighlight } from "@/lib/customLang";
+import { testingLang } from "@/lib/testingLang";
+import { lowlight } from "lowlight";
 import Head from "next/head";
 import { useRouter } from "next/router";
 import React, { useEffect } from "react";
@@ -23,13 +30,32 @@ const useStyles = createStyles((theme) => ({
   topInputs: {
     maxWidth: "max(50%,300px)",
   },
+  editor: {
+    fontSize: "1.2rem",
+
+    "& > code": {
+      fontSize: "1.2rem",
+    },
+  },
 }));
+
+lowlight.registerLanguage("cb", testingLang);
 
 function Create() {
   const { classes, cx } = useStyles();
 
   const router = useRouter();
   const { id } = router.query;
+
+  const editor = useEditor({
+    extensions: [
+      StarterKit,
+      CodeBlockLowlight.configure({
+        lowlight,
+      }),
+    ],
+    content: "<pre><code></code></pre>",
+  });
 
   const form = useForm({
     initialValues: {
@@ -83,13 +109,48 @@ function Create() {
               maxRows={5}
             />
             <Center>
-            <Button type="submit" color="blue" leftIcon={<IconDeviceFloppy />}>
-              Save Bot
-            </Button>
-          </Center>
-          <Text size="lg">Add Code Below</Text>
+              <Button
+                type="submit"
+                color="blue"
+                leftIcon={<IconDeviceFloppy />}
+              >
+                Save Bot
+              </Button>
+            </Center>
+            <Text size="lg">Add Code Below</Text>
+            <RichTextEditor
+              editor={editor}
+              classNames={{ content: classes.editor }}
+              styles={(theme) => ({
+                content: {
+                  pre: {
+                    background: theme.colors.dark[8],
+                    borderRadius: theme.fn.radius(),
+                    color: theme.colors.dark[0],
+                    fontFamily: theme.fontFamilyMonospace,
+
+                    "& code": {
+                      background: "none",
+                      color: "inherit",
+                      fontSize: theme.fontSizes.md,
+                      padding: 0,
+                    },
+                    " & .hljs-comment, & .hljs-quote": {
+                      color: theme.colors.green[3],
+                    },
+                  },
+                },
+              })}
+            >
+              <RichTextEditor.Toolbar>
+                <RichTextEditor.ControlsGroup>
+                  <RichTextEditor.CodeBlock />
+                </RichTextEditor.ControlsGroup>
+              </RichTextEditor.Toolbar>
+
+              <RichTextEditor.Content />
+            </RichTextEditor>
           </Container>
-          
         </form>
       </main>
     </>
